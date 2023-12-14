ca739daea1ff0a23a4e4204a4522522bd693bafe
SURESH <sureshsaripalle@gmail.com>
2023-12-14 15:30:40 +0530

    First commit

---
+++ src/main/java/com/app/demo/MavenJavaJenkinsProject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.MavenJavaJenkinsProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Creating a CI/CD pipeline using Jenkins for a sample Maven java project." );
+    }
+}
